267f5aff3218dc9aaf4975a2f8629dfd867986dd
Paul Pearson <pauldarlingtonpearson@gmail.com>
2018-10-14 18:41:41 -0400

    brought back the IndexController to keep up with sfg

---
+++ src/main/java/com/cellar/wine/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.cellar.wine.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+
+}
