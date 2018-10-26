90b64ffd880cd4c22c788cbc0a00a3e846e9d302
Paul Pearson <pauldarlingtonpearson@gmail.com>
2018-10-25 20:40:25 -0400

    updated the gradle wrapper to 4.10.2; few other minor changes

--- src/main/java/com/cellar/wine/services/WineService.java
+++ src/main/java/com/cellar/wine/services/WineService.java
@@ -2,8 +2,6 @@ package com.cellar.wine.services;
 
 import com.cellar.wine.model.Wine;
 
-import java.util.Set;
-
 public interface WineService extends CrudService<Wine, Long> {
 
     Wine findByName(String name);
