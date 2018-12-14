68662d8254712803d3f51b2b8d8aea7e344788cd
Paul Pearson <pauldarlingtonpearson@gmail.com>
2018-12-13 19:06:17 -0500

    removed unnecessary properties files

--- src/main/java/com/cellar/wine/controllers/IndexController.java
+++ src/main/java/com/cellar/wine/controllers/IndexController.java
@@ -11,7 +11,7 @@ public class IndexController {
         return "index";
     }
 
-    @RequestMapping("/error")
+    @RequestMapping("/errorpage")
     public String error() {
         return "error";
     }
