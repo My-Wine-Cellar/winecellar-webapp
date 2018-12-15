4ac55288935af3e814300fec62478b13db46d680
Paul Pearson <pauldarlingtonpearson@gmail.com>
2018-12-15 14:12:48 -0500

    implemented find producers

--- src/main/java/com/cellar/wine/controllers/ProducerController.java
+++ src/main/java/com/cellar/wine/controllers/ProducerController.java
@@ -1,12 +1,17 @@
 package com.cellar.wine.controllers;
 
+import com.cellar.wine.models.Producer;
 import com.cellar.wine.services.ProducerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("/producers")
 @Controller
@@ -23,7 +28,7 @@ public class ProducerController {
         dataBinder.setAllowedFields("id");
     }
 
-    @RequestMapping("/producerlist")
+    @RequestMapping("/list")
     public String producer(Model model) {
         model.addAttribute("producers", producerService.findAll());
         return "producers/index";
@@ -35,6 +40,35 @@ public class ProducerController {
         return "producers/details";
     }
 
+    @RequestMapping("/find")
+    public String findProducers(Model model, Producer producer) {
+        model.addAttribute("producer",  producer);
+        return "producers/findProducers";
+    }
 
+    @GetMapping("")
+    public String processFindForm(Model model, BindingResult result, Producer producer) {
+        //allow parameterless GET request for producers to return all records
+        if (producer.getName() == null) {
+            producer.setName(""); //empty string signifies broadest possible search
+        }
+
+        //find producers by name
+        List<Producer> results = producerService.findAllByNameLike(producer.getName());
+
+        if (results.isEmpty()) {
+            //no producers found
+            result.rejectValue("name", "notFound", "Not Found");
+            return "producers/findProducers";
+        } else if (results.size() == 1) {
+            //1 producer found
+            producer = results.iterator().next();
+            return "redirect:/producers/" + producer.getId();
+        } else {
+            //multiple producers found
+            model.addAttribute("selections", results);
+            return "producers/producersList";
+        }
+    }
 
 }
