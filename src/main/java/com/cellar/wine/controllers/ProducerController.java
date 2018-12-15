95822c57307ec6f28166938ac107b3474f26348e
Paul Pearson <pauldarlingtonpearson@gmail.com>
2018-12-14 19:46:16 -0500

    added producer details view, shows producer and its list(table) of wines

--- src/main/java/com/cellar/wine/controllers/ProducerController.java
+++ src/main/java/com/cellar/wine/controllers/ProducerController.java
@@ -3,6 +3,7 @@ package com.cellar.wine.controllers;
 import com.cellar.wine.services.ProducerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/producers")
@@ -19,6 +20,12 @@ public class ProducerController {
     public String producer(Model model) {
         model.addAttribute("producers", producerService.findAll());
         return "producers/index";
+    }
+
+    @RequestMapping("/{producerId}")
+    public String producerDetails(@PathVariable("producerId") Long id, Model model) {
+        model.addAttribute("producerId", producerService.findById(new Long(id)));
+        return "producers/details";
     }
 
 }
