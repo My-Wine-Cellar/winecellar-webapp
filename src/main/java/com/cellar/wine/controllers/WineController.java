61208708cc017db114be788473dcbce00cd3b7b7
Paul Pearson <pauldarlingtonpearson@gmail.com>
2018-12-29 14:38:49 -0500

    save wine is now working; cleaned up wine forms and buttons

--- src/main/java/com/cellar/wine/controllers/WineController.java
+++ src/main/java/com/cellar/wine/controllers/WineController.java
@@ -1,10 +1,16 @@
 package com.cellar.wine.controllers;
 
+import com.cellar.wine.models.Wine;
 import com.cellar.wine.services.ProducerService;
 import com.cellar.wine.services.WineService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @RequestMapping("/wines")
 @Controller
@@ -24,5 +30,21 @@ public class WineController {
         return "wines/index";
     }
 
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+        Wine wine = new Wine();
+        model.addAttribute("addwine", wine);
+        return "wines/create-wine";
+    }
+
+    @PostMapping("/saveWine")
+    public String saveForm(@Valid Wine wine, BindingResult result) {
+        if (result.hasErrors()) {
+            return "/wines";
+        } else {
+            wineService.save(wine);
+            return "redirect:/wines/list";
+        }
+    }
 
 }
