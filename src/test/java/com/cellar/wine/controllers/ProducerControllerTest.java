e4746396ec566c5e2639124a3c4445ded8bc51e5
Paul Pearson <pauldarlingtonpearson@gmail.com>
2019-01-01 16:47:20 -0500

    adding tests for controllers and services

--- src/test/java/com/cellar/wine/controllers/ProducerControllerTest.java
+++ src/test/java/com/cellar/wine/controllers/ProducerControllerTest.java
@@ -2,13 +2,26 @@ package com.cellar.wine.controllers;
 
 import com.cellar.wine.models.Producer;
 import com.cellar.wine.services.ProducerService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.HashSet;
 import java.util.Set;
+
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ProducerControllerTest {
@@ -23,4 +36,75 @@ public class ProducerControllerTest {
 
     MockMvc mockMvc;
 
+    Producer producer1 = new Producer();
+    Producer producer2 = new Producer();
+
+    @BeforeEach
+    void setUp() {
+
+        producers = new HashSet<>();
+        producers.add(producer1);
+        producers.add(producer2);
+
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(producerController)
+                .build();
+    }
+
+    @Test
+    void displayOwner() throws Exception {
+        when(producerService.findById(anyLong())).thenReturn(producer1);
+
+        mockMvc.perform(get("/producer/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("producers/details"))
+                .andExpect(model().attribute("producer", hasProperty("id", is(1l))));
+    }
+
+
+    @Test
+    void initCreationForm() throws Exception {
+        mockMvc.perform(get("/producers/new"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("producers/createOrUpdateProducer"))
+                .andExpect(model().attributeExists("producer"));
+
+        verifyZeroInteractions(producerService);
+    }
+
+    @Test
+    void processCreationForm() throws Exception {
+        when(producerService.save(ArgumentMatchers.any())).thenReturn(producer1);
+
+        mockMvc.perform(post("/producers/new"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/producers/1"))
+                .andExpect(model().attributeExists("producers"));
+
+        verify(producerService).save(ArgumentMatchers.any());
+    }
+
+    @Test
+    void initUpdateProducerForm() throws Exception {
+        when(producerService.findById(anyLong())).thenReturn(producer1);
+
+        mockMvc.perform(get("/producers/1/edit"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("producers/createOrUpdateProducer"))
+                .andExpect(model().attributeExists("producer"));
+
+        verifyZeroInteractions(producerService);
+    }
+
+    @Test
+    void processUpdateProducerForm() throws Exception {
+        when(producerService.save(ArgumentMatchers.any())).thenReturn(producer1);
+
+        mockMvc.perform(post("/producers/1/edit"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/producers/{producerId}"))
+                .andExpect(model().attributeExists("producer"));
+
+        verify(producerService).save(ArgumentMatchers.any());
+    }
 }
--- src/test/java/com/cellar/wine/controllers/WineControllerTest.java
+++ src/test/java/com/cellar/wine/controllers/WineControllerTest.java
@@ -1,17 +1,25 @@
 package com.cellar.wine.controllers;
 
+import com.cellar.wine.models.Producer;
 import com.cellar.wine.models.Wine;
+import com.cellar.wine.services.ProducerService;
 import com.cellar.wine.services.WineService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.Set;
-
-import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 public class WineControllerTest {
@@ -19,11 +27,69 @@ public class WineControllerTest {
     @Mock
     WineService wineService;
 
+    @Mock
+    ProducerService producerService;
+
     @InjectMocks
     WineController wineController;
 
-    Set<Wine> wines;
-
     MockMvc mockMvc;
+
+    Producer producer;
+    Wine wine;
+
+    @BeforeEach
+    void setUp() {
+        producer = new Producer();
+
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(wineController)
+                .build();
+    }
+
+    @Test
+    void initCreationForm() throws Exception {
+        when(producerService.findById(anyLong())).thenReturn(producer);
+
+        mockMvc.perform(get("/producers/1/wines/new"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("producer"))
+                .andExpect(model().attributeExists("wine"))
+                .andExpect(view().name("wines/createOrUpdateWine"));
+    }
+
+    @Test
+    void processCreationForm() throws Exception {
+        when(producerService.findById(anyLong())).thenReturn(producer);
+
+        mockMvc.perform(post("/producers/1/wines/new"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/producers/1"));
+    }
+
+    @Test
+    void initUpdateForm() throws Exception {
+        when(producerService.findById(anyLong())).thenReturn(producer);
+
+        when(wineService.findById(anyLong())).thenReturn(wine);
+
+        mockMvc.perform(get("/producers/1/wines/2/edit"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("producer"))
+                .andExpect(model().attributeExists("wine"))
+                .andExpect(view().name("wines/createOrUpdateWine"));
+    }
+
+    @Test
+    void processUpdateForm() throws Exception {
+        when(producerService.findById(anyLong())).thenReturn(producer);
+
+        mockMvc.perform(post("/producers/1/wines/2/edit"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/producers/1"));
+
+        verify(wineService).save(any());
+    }
+
 
 }
