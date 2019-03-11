7769ba2700d37e6542ebe5a7a39b07c74a872770
Paul Pearson <pauldarlingtonpearson@gmail.com>
2019-03-11 15:13:54 -0400

    added war plugin, changd version to 0.0.6

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO authority (id,authority) VALUES (1,'ADMIN');
-INSERT INTO authority (id,authority) VALUES (2,'USER');
+INSERT INTO authority (id,authority) VALUES (1,'ROLE_ADMIN');
+INSERT INTO authority (id,authority) VALUES (2,'ROLE_USER');
