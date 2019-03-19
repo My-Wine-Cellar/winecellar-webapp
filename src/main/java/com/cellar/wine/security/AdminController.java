f9d851b2966c02ad2d83b90fd7acfb2251e0130d
Paul Pearson <pauldarlingtonpearson@gmail.com>
2019-03-18 20:00:01 -0400

    added ability to register admin

--- src/main/java/com/cellar/wine/security/AdminController.java
+++ src/main/java/com/cellar/wine/security/AdminController.java
@@ -6,34 +6,29 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 
 @Controller
-public class LoginController {
+public class AdminController {
 
     private UserService userService;
 
-    public LoginController(UserService userService) {
+    public AdminController(UserService userService) {
         this.userService = userService;
     }
 
-    private static final String SECURITY_REGISTRATION_TEMPLATE = "security/registration";
+    private static final String USER_REGISTRATION_TEMPLATE = "security/registerUser";
+    private static final String ADMIN_REGISTRATION_TEMPLATE = "security/registerAdmin";
 
-    @RequestMapping("/login")
-    public String loginForm() {
-        return "security/login";
-    }
-
-    @GetMapping("/registration")
-    public String registration(Model model) {
+    @GetMapping("/registeruser")
+    public String registerUser(Model model) {
         model.addAttribute("user", User.builder().build());
-        return SECURITY_REGISTRATION_TEMPLATE;
+        return USER_REGISTRATION_TEMPLATE;
     }
 
-    @PostMapping("/registration")
+    @PostMapping("/registeruser")
     public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
         ModelAndView modelAndView = new ModelAndView();
         User userExists = userService.findByUsername(user.getUsername());
@@ -43,12 +38,38 @@ public class LoginController {
                             "There is already a user registered with the username provided");
         }
         if (bindingResult.hasErrors()) {
-            modelAndView.setViewName(SECURITY_REGISTRATION_TEMPLATE);
+            modelAndView.setViewName(USER_REGISTRATION_TEMPLATE);
         } else {
             userService.createUser(user);
             modelAndView.addObject("successMessage", "User has been registered successfully");
             modelAndView.addObject("user", new User());
-            modelAndView.setViewName(SECURITY_REGISTRATION_TEMPLATE);
+            modelAndView.setViewName(USER_REGISTRATION_TEMPLATE);
+        }
+        return modelAndView;
+    }
+
+    @GetMapping("/registeradmin")
+    public String registerAdmin(Model model) {
+        model.addAttribute("admin", User.builder().build());
+        return ADMIN_REGISTRATION_TEMPLATE;
+    }
+
+    @PostMapping("/registeradmin")
+    public ModelAndView createNewAdmin(@Valid User admin, BindingResult bindingResult) {
+        ModelAndView modelAndView = new ModelAndView();
+        User adminExists = userService.findByUsername(admin.getUsername());
+        if (adminExists != null) {
+            bindingResult
+                    .rejectValue("username", "error.user",
+                            "There is already an admin registered with the username provided");
+        }
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName(ADMIN_REGISTRATION_TEMPLATE);
+        } else {
+            userService.createAdmin(admin);
+            modelAndView.addObject("successMessage", "Admin has been registered successfully");
+            modelAndView.addObject("admin", new User());
+            modelAndView.setViewName(ADMIN_REGISTRATION_TEMPLATE);
         }
         return modelAndView;
     }
