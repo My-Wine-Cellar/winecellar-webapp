f000dd6f9c23392dc2e4f1f3377766207c971543
Paul Pearson <pauldarlingtonpearson@gmail.com>
2019-03-25 16:37:57 -0400

    application properties are ready for test,dev,prod

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO authority (id,authority) VALUES (1,'ROLE_ADMIN');
 INSERT INTO authority (id,authority) VALUES (2,'ROLE_USER');
+
+INSERT INTO user (id, enabled, username, password) VALUES ( 999999, true, 'paulpearson', '$2a$10$/GlmRbPOiK31IhckttEeFuNadoELtgzixJVquUQR3xhlv9HfOjy5q');
+
+INSERT INTO user_authority (authority_id, user_id) VALUES ( 1, 999999 );
