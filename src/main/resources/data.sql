0c300ecf0dc638bc720522673c049cc16105faed
Paul Pearson <pauldarlingtonpearson@gmail.com>
2019-03-27 12:36:47 -0400

    adding user for dev testing, turned off show sql for prod

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,7 @@ INSERT INTO authority (id,authority) VALUES (2,'ROLE_USER');
 INSERT INTO user (id, enabled, username, password) VALUES ( 999999, true, 'paulpearson', '$2a$10$/GlmRbPOiK31IhckttEeFuNadoELtgzixJVquUQR3xhlv9HfOjy5q');
 
 INSERT INTO user_authority (authority_id, user_id) VALUES ( 1, 999999 );
+
+INSERT INTO user (id, enabled, username, password) VALUES ( 888888, true, 'pauldpearson', '$2a$10$/GlmRbPOiK31IhckttEeFuNadoELtgzixJVquUQR3xhlv9HfOjy5q');
+
+INSERT INTO user_authority (authority_id, user_id) VALUES ( 2, 888888 );
