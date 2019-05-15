896beb3a37915c869b178799f773ea63e9662932
Paul Pearson <pauldarlingtonpearson@gmail.com>
2019-05-15 10:10:31 -0400

    refactored sql scripts and properties file for db initialization

---
+++ src/main/resources/sql/security.sql
@@ -0,0 +1,10 @@
+INSERT INTO authority (id,authority) VALUES (1,'ROLE_ADMIN');
+INSERT INTO authority (id,authority) VALUES (2,'ROLE_USER');
+
+INSERT INTO users (id, enabled, username, password) VALUES ( 999999, true, 'admin', '$2a$10$FQvwDTU8onTZ74SJy1N2zOLAeIC5em9ETr5ggcPS9/Ne/S4WXM2zG');
+
+INSERT INTO user_authority (authority_id, user_id) VALUES ( 1, 999999 );
+
+INSERT INTO users (id, enabled, username, password) VALUES ( 888888, true, 'user', '$2a$10$FQvwDTU8onTZ74SJy1N2zOLAeIC5em9ETr5ggcPS9/Ne/S4WXM2zG');
+
+INSERT INTO user_authority (authority_id, user_id) VALUES ( 2, 888888 );
