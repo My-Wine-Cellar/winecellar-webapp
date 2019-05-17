74fd05a317a3810177f1d2ed0ddf1e5799f1c3dc
Paul Pearson <pauldarlingtonpearson@gmail.com>
2019-05-17 15:33:46 -0400

    country view is working, not sexy yet

---
+++ src/main/java/com/cellar/wine/controllers/CountryController.java
@@ -0,0 +1,26 @@
+package com.cellar.wine.controllers;
+
+import com.cellar.wine.services.RegionService;
+import com.cellar.wine.services.impl.CountryServiceImpl;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/country")
+public class CountryController {
+
+    private CountryServiceImpl countryService;
+    private RegionService regionService;
+
+    public CountryController(CountryServiceImpl countryService, RegionService regionService) {
+        this.countryService = countryService;
+        this.regionService = regionService;
+    }
+
+    @RequestMapping("/list")
+    public String getAllCountriesWithRegions(Model model) {
+        model.addAttribute("countries", countryService.findAll());
+        return "country/countryList";
+    }
+}
