46ecc2aa145a9576ab86b987656f6ae8becf21cc
jesperpedersen <jesper.pedersen@comcast.net>
2019-06-09 06:59:08 -0400

    Add harvest_begin and harvest_end

--- src/main/java/com/cellar/wine/models/GrapeComponent.java
+++ src/main/java/com/cellar/wine/models/GrapeComponent.java
@@ -2,6 +2,7 @@ package com.cellar.wine.models;
 
 import lombok.*;
 
+import java.sql.Date;
 import java.util.List;
 import javax.persistence.*;
 
@@ -14,15 +15,23 @@ import javax.persistence.*;
 public class GrapeComponent extends BaseEntity implements Comparable<GrapeComponent> {
 
     @Builder
-    public GrapeComponent(Long id, Byte percentage, Grape grape, Wine wine) {
+    public GrapeComponent(Long id, Byte percentage, Date harvestBegin, Date harvestEnd, Grape grape, Wine wine) {
         super(id);
         this.percentage = percentage;
+        this.harvestBegin = harvestBegin;
+        this.harvestEnd = harvestEnd;
         this.grape = grape;
         this.wine = wine;
     }
 
     @Column(name = "percentage")
     private Byte percentage;
+
+    @Column(name = "harvest_begin")
+    private Date harvestBegin;
+
+    @Column(name = "harvest_end")
+    private Date harvestEnd;
 
     @ManyToOne
     @JoinColumn(name = "grape_id", referencedColumnName = "id")
--- src/main/resources/sql/99_sample.sql
+++ src/main/resources/sql/99_sample.sql
@@ -20,14 +20,14 @@ INSERT INTO area_producer (area_id, producer_id) VALUES (704, 2);
 INSERT INTO wine (name, vintage, size, alcohol, bottle_aging, producer_id) VALUES ('Brunello di Montalcino', 2013, 0.75, 14.0, 240, 2);
 INSERT INTO maceration (days) VALUES (8);
 INSERT INTO fermentation (days, temperature) VALUES (20, 26);
-INSERT INTO grape_component (percentage, grape_id, wine_id, maceration_id, fermentation_id) VALUES (100, 201, 2, 2, 1);
+INSERT INTO grape_component (percentage, harvest_begin, harvest_end, grape_id, wine_id, maceration_id, fermentation_id) VALUES (100, '2013-09-26'::date, '2013-10-21'::date, 201, 2, 2, 1);
 INSERT INTO barrel_component (percentage, aging, grape_component_id, barrel_id) VALUES (100, 1095, 6, 5);
 -- Madre
 INSERT INTO wine (name, vintage, size, alcohol, bottle_aging, producer_id) VALUES ('Madre', 2016, 0.75, 14.5, 120, 2);
 INSERT INTO fermentation (days, temperature) VALUES (10, 30);
 INSERT INTO fermentation (days, temperature) VALUES (9, 28);
-INSERT INTO grape_component (percentage, grape_id, wine_id, fermentation_id) VALUES (50, 201, 3, 2);
-INSERT INTO grape_component (percentage, grape_id, wine_id, fermentation_id) VALUES (50, 116, 3, 3);
+INSERT INTO grape_component (percentage, harvest_begin, harvest_end, grape_id, wine_id, fermentation_id) VALUES (50, '2016-09-22'::date, '2016-10-07'::date, 201, 3, 2);
+INSERT INTO grape_component (percentage, harvest_begin, harvest_end, grape_id, wine_id, fermentation_id) VALUES (50, '2016-09-22'::date, '2016-10-07'::date, 116, 3, 3);
 INSERT INTO barrel_component (percentage, aging, size, grape_component_id, barrel_id) VALUES (100, 540, 500, 7, 1);
 INSERT INTO barrel_component (percentage, aging, size, grape_component_id, barrel_id) VALUES (100, 540, 500, 8, 1);
 
