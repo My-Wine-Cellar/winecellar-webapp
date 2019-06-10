12db6e8d97370fec3b84870f995912697e6be4c9
Paul Pearson <pauldarlingtonpearson@gmail.com>
2019-06-10 19:03:34 -0400

    working on add/edit producer

--- src/main/java/com/cellar/wine/controllers/ProducerController.java
+++ src/main/java/com/cellar/wine/controllers/ProducerController.java
@@ -1,11 +1,14 @@
 package com.cellar.wine.controllers;
 
+import com.cellar.wine.models.Producer;
 import com.cellar.wine.services.impl.ProducerServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/producer")
@@ -17,6 +20,15 @@ public class ProducerController {
         this.producerService = producerService;
     }
 
+    private static final String ADD_OR_EDIT_PRODUCER_TEMPLATE = "producer/addEditProducer";
+
+    private static final String MODEL_ATTRIBUTE_PRODUCER = "producer";
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
     @GetMapping("/list")
     public String showAllProducers(Model model) {
         model.addAttribute("producers", producerService.findAll());
@@ -25,7 +37,40 @@ public class ProducerController {
 
     @GetMapping("/{producerId}")
     public String producerDetails(@PathVariable Long producerId, Model model) {
-        model.addAttribute("producer", producerService.findById(producerId));
+        model.addAttribute(MODEL_ATTRIBUTE_PRODUCER, producerService.findById(producerId));
         return "producer/producerDetails";
+    }
+
+    @GetMapping("/new")
+    public String initCreateForm(Model model) {
+        model.addAttribute(MODEL_ATTRIBUTE_PRODUCER, Producer.builder().build());
+        return ADD_OR_EDIT_PRODUCER_TEMPLATE;
+    }
+
+    @PostMapping("/new")
+    public String processCreateForm(@Valid Producer producer, BindingResult result) {
+        if (result.hasErrors()) {
+            return ADD_OR_EDIT_PRODUCER_TEMPLATE;
+        } else {
+            Producer savedProducer = producerService.save(producer);
+            return "redirect:/producers/" + savedProducer.getId();
+        }
+    }
+
+    @GetMapping("/{producerId}/edit")
+    public String initAddEditProducerForm(@PathVariable Long producerId, Model model) {
+        model.addAttribute(producerService.findById(producerId));
+        return ADD_OR_EDIT_PRODUCER_TEMPLATE;
+    }
+
+    @PostMapping("/{producerId}/edit")
+    public String processAddEditProducerForm(@Valid Producer producer, BindingResult result, @PathVariable Long producerId) {
+        if(result.hasErrors()) {
+            return ADD_OR_EDIT_PRODUCER_TEMPLATE;
+        } else {
+            producer.setId(producerId);
+            Producer savedProducer = producerService.save(producer);
+            return "redirect:/producers/" + savedProducer.getId();
+        }
     }
 }
