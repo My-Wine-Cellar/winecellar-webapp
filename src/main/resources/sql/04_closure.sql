85b3123a4c852e7571b8fb6e4ec7a72d71c9a06a
jesperpedersen <jesper.pedersen@comcast.net>
2019-07-06 06:59:57 -0400

    Add valve as a closure

--- src/main/resources/sql/04_closure.sql
+++ src/main/resources/sql/04_closure.sql
@@ -1,5 +1,6 @@
 INSERT INTO closure (name, description, weblink) VALUES ('Natural Cork', '', '');
 INSERT INTO closure (name, description, weblink) VALUES ('Synthetic Cork', '', '');
 INSERT INTO closure (name, description, weblink) VALUES ('Screw Cap', '', '');
+INSERT INTO closure (name, description, weblink) VALUES ('Valve', '', '');
 INSERT INTO closure (name, description, weblink) VALUES ('Vino-Seal', '', '');
 INSERT INTO closure (name, description, weblink) VALUES ('Zork', '', '');
