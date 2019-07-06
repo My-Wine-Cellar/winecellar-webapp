b0e68ad22913828e365d6bf7284f71cde3dc7407
jesperpedersen <jesper.pedersen@comcast.net>
2019-07-06 07:32:07 -0400

    Add bottle shape

---
+++ src/main/resources/sql/05_shape.sql
@@ -0,0 +1,8 @@
+INSERT INTO shape (name, description, weblink) VALUES ('Bordeaux', '', '');
+INSERT INTO shape (name, description, weblink) VALUES ('Burgundy', '', '');
+INSERT INTO shape (name, description, weblink) VALUES ('Alsace', '', '');
+INSERT INTO shape (name, description, weblink) VALUES ('California', '', '');
+INSERT INTO shape (name, description, weblink) VALUES ('Rhône', '', '');
+INSERT INTO shape (name, description, weblink) VALUES ('Champagne', '', '');
+INSERT INTO shape (name, description, weblink) VALUES ('Box', '', '');
+INSERT INTO shape (name, description, weblink) VALUES ('Other', '', '');
--- src/main/resources/sql/99_sample.sql
+++ src/main/resources/sql/99_sample.sql
@@ -4,7 +4,7 @@ INSERT INTO grape_area (grape_id, area_id) VALUES (201, 704);
 -- Opus One Winery / Opus One 2015
 INSERT INTO producer (name, website) VALUES ('Opus One Winery', 'https://www.opusonewinery.com/');
 INSERT INTO area_producer (area_id, producer_id) VALUES (80, 1);
-INSERT INTO wine (name, vintage, size, weblink, producer_id, closure_id) VALUES ('Opus One', 2015, 0.75, 'https://www.opusonewinery.com/wine/', 1, 1);
+INSERT INTO wine (name, vintage, size, weblink, producer_id, closure_id, shape_id) VALUES ('Opus One', 2015, 0.75, 'https://www.opusonewinery.com/wine/', 1, 1, 4);
 INSERT INTO maceration (days) VALUES (21);
 INSERT INTO grape_component (percentage, grape_id, wine_id, maceration_id) VALUES (81, 116, 1, 1);
 INSERT INTO grape_component (percentage, grape_id, wine_id) VALUES (7, 115, 1);
@@ -17,13 +17,13 @@ INSERT INTO barrel_component (percentage, aging, grape_component_id, barrel_id) 
 INSERT INTO producer (name, phone, fax, email, description, website) VALUES ('Poggio Antico', '+39 0577-848044', '+39 0577-846563', 'mail@poggioantico.com', 'Poggio Antico, Tuscany', 'https://www.poggioantico.com/');
 INSERT INTO area_producer (area_id, producer_id) VALUES (704, 2);
 -- Brunello di Montalcino
-INSERT INTO wine (name, vintage, size, alcohol, acid, bottle_aging, weblink, producer_id, closure_id) VALUES ('Brunello di Montalcino', 2013, 0.75, 14.0, 5.6, 240, 'https://www.poggioantico.com/product/brunello-di-montalcino/', 2, 1);
+INSERT INTO wine (name, vintage, size, alcohol, acid, bottle_aging, weblink, producer_id, closure_id, shape_id) VALUES ('Brunello di Montalcino', 2013, 0.75, 14.0, 5.6, 240, 'https://www.poggioantico.com/product/brunello-di-montalcino/', 2, 1, 1);
 INSERT INTO maceration (days) VALUES (8);
 INSERT INTO fermentation (days, temperature) VALUES (20, 26);
 INSERT INTO grape_component (percentage, harvest_begin, harvest_end, grape_id, wine_id, maceration_id, fermentation_id) VALUES (100, '2013-09-26'::date, '2013-10-21'::date, 201, 2, 2, 1);
 INSERT INTO barrel_component (percentage, aging, grape_component_id, barrel_id) VALUES (100, 1095, 6, 5);
 -- Madre
-INSERT INTO wine (name, vintage, size, alcohol, acid, bottle_aging, weblink, producer_id, closure_id) VALUES ('Madre', 2016, 0.75, 14.5, 5.7, 120, 'https://www.poggioantico.com/product/madre/', 2, 1);
+INSERT INTO wine (name, vintage, size, alcohol, acid, bottle_aging, weblink, producer_id, closure_id, shape_id) VALUES ('Madre', 2016, 0.75, 14.5, 5.7, 120, 'https://www.poggioantico.com/product/madre/', 2, 1, 1);
 INSERT INTO fermentation (days, temperature) VALUES (10, 30);
 INSERT INTO fermentation (days, temperature) VALUES (9, 28);
 INSERT INTO grape_component (percentage, harvest_begin, harvest_end, grape_id, wine_id, fermentation_id) VALUES (50, '2016-09-22'::date, '2016-10-07'::date, 201, 3, 2);
