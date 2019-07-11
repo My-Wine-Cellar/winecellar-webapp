6b150063d8be10bb549d77d6fe924fdf61e08f5c
Paul Pearson <pauldarlingtonpearson@gmail.com>
2019-07-11 09:36:23 -0400

    MVC: edit region close #104

--- src/main/java/com/cellar/wine/controllers/RegionController.java
+++ src/main/java/com/cellar/wine/controllers/RegionController.java
@@ -1,11 +1,16 @@
 package com.cellar.wine.controllers;
 
+import com.cellar.wine.models.Region;
 import com.cellar.wine.services.RegionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/region")
@@ -17,9 +22,26 @@ public class RegionController {
         this.regionService = regionService;
     }
 
-    @GetMapping("{regionId}")
+    @GetMapping("/{regionId}")
     public String regionDetails(@PathVariable Long regionId, Model model) {
         model.addAttribute("region", regionService.findById(regionId));
         return "region/regionDetails";
+    }
+
+    @GetMapping("/{regionId}/edit")
+    public String initEditRegionForm(@PathVariable Long regionId, Model model) {
+        model.addAttribute("region", regionService.findById(regionId));
+        return "region/editRegion";
+    }
+
+    @PostMapping("/{regionId}/edit")
+    public String processEditRegionForm(@Valid Region region, BindingResult result, @PathVariable Long regionId) {
+        if(result.hasErrors()) {
+            return "region/editRegion";
+        } else {
+            region.setId(regionId);
+            regionService.save(region);
+            return "redirect:/region/" + regionId;
+        }
     }
 }
