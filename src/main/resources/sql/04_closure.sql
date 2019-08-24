51de478f686b89bf9e74f8ab96e1517589d43d36
jesperpedersen <jesper.pedersen@comcast.net>
2019-08-24 13:38:21 -0400

    Add technical cork, and other

--- src/main/resources/sql/04_closure.sql
+++ src/main/resources/sql/04_closure.sql
@@ -1,6 +1,8 @@
 INSERT INTO closure (name, description, weblink) VALUES ('Natural Cork', '', '');
+INSERT INTO closure (name, description, weblink) VALUES ('Technical Cork', '', '');
 INSERT INTO closure (name, description, weblink) VALUES ('Synthetic Cork', '', '');
 INSERT INTO closure (name, description, weblink) VALUES ('Screw Cap', '', '');
 INSERT INTO closure (name, description, weblink) VALUES ('Valve', '', '');
 INSERT INTO closure (name, description, weblink) VALUES ('Vino-Seal', '', '');
 INSERT INTO closure (name, description, weblink) VALUES ('Zork', '', '');
+INSERT INTO closure (name, description, weblink) VALUES ('Other', '', '');
