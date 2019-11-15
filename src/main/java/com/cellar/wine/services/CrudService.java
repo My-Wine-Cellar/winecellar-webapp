006bbfe420ccf671eb0815b39dc888e43814ceb6
jesperpedersen <jesper.pedersen@redhat.com>
2019-11-15 06:58:03 -0500

    checkstyle

--- src/main/java/com/cellar/wine/services/CrudService.java
+++ src/main/java/com/cellar/wine/services/CrudService.java
@@ -1,3 +1,11 @@
+/*
+ * My-Wine-Cellar, copyright 2019
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ */
+
 package com.cellar.wine.services;
 
 public interface CrudService<T, ID> {
