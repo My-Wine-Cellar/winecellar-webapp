c6815ad2eadfc986b24992bcc91c4941840151c4
jesperpedersen <jesper.pedersen@redhat.com>
2019-11-25 07:23:33 -0500

    [#171] checkstyle

--- src/main/java/info/mywinecellar/nav/Paths.java
+++ src/main/java/info/mywinecellar/nav/Paths.java
@@ -10,74 +10,132 @@ package info.mywinecellar.nav;
 
 public class Paths {
 
+    /** ABOUT */
     public static final String ABOUT = "about/about";
+    /** ABOUT_CONTACT_US */
     public static final String ABOUT_CONTACT_US = "about/contactus";
+    /** ABOUT_CREDITS */
     public static final String ABOUT_CREDITS = "about/credits";
+    /** ABOUT_LICENSE */
     public static final String ABOUT_LICENSE = "about/license";
+    /** ABOUT_PEOPLE */
     public static final String ABOUT_PEOPLE = "about/people";
+    /** AREA_ADD_GRAPE */
     public static final String AREA_ADD_GRAPE = "area/areaAddGrape";
+    /** AREA_DETAILS */
     public static final String AREA_DETAILS = "area/areaDetails";
+    /** AREA_EDIT */
     public static final String AREA_EDIT = "area/areaEdit";
 
+    /** BARREL_DETAILS */
     public static final String BARREL_DETAILS = "barrel/barrelDetails";
+    /** BARREL_EDIT */
     public static final String BARREL_EDIT = "barrel/barrelEdit";
-
+    /** BOTTLE_ADD_EDIT */
     public static final String BOTTLE_ADD_EDIT = "bottle/bottleAddEdit";
+    /** BOTTLE_LIST */
     public static final String BOTTLE_LIST = "bottle/bottleList";
 
+    /** COUNTRY_DETAILS */
     public static final String COUNTRY_DETAILS = "country/countryDetails";
+    /** COUNTRY_EDIT */
     public static final String COUNTRY_EDIT = "country/countryEdit";
+    /** COUNTRY_LIST */
     public static final String COUNTRY_LIST = "country/countryList";
 
+    /** ERROR_PAGE */
     public static final String ERROR_PAGE = "error";
 
+    /** GRAPE_DETAILS */
     public static final String GRAPE_DETAILS = "grape/grapeDetails";
+    /** GRAPE_EDIT */
     public static final String GRAPE_EDIT = "grape/grapeEdit";
+    /** GRAPE_LIST */
     public static final String GRAPE_LIST = "grape/grapeList";
 
+    /** LANDING_PAGE */
     public static final String LANDING_PAGE = "index/landingPage";
-    
+
+    /** PRODUCER_DETAILS */
     public static final String PRODUCER_DETAILS = "producer/producerDetails";
+    /** PRODUCER_ADD_EDIT */
     public static final String PRODUCER_ADD_EDIT = "producer/producerAddEdit";
-    
+
+    /** REDIRECT_ADMIN_USER_LIST */
     public static final String REDIRECT_ADMIN_USER_LIST = "redirect:/admin/userlist";
+    /** REDIRECT_BARREL */
     public static final String REDIRECT_BARREL = "redirect:/barrel/";
+    /** REDIRECT_BOTTLE_LIST */
     public static final String REDIRECT_BOTTLE_LIST = "redirect:/bottle/list";
+    /** REDIRECT_GRAPE */
     public static final String REDIRECT_GRAPE = "redirect:/grape/";
+    /** REDIRECT_REGISTER_SUCCESS */
     public static final String REDIRECT_REGISTER_SUCCESS = "redirect:/register/success";
+    /** REDIRECT_REVIEW_LIST */
     public static final String REDIRECT_REVIEW_LIST = "redirect:/review/list";
+    /** REDIRECT_ROOT */
     public static final String REDIRECT_ROOT = "redirect:/";
+    /** REDIRECT_TASTED_LIST */
     public static final String REDIRECT_TASTED_LIST = "redirect:/tasted/list";
+    /** REDIRECT_TASTINGNOTES_LIST */
     public static final String REDIRECT_TASTINGNOTES_LIST = "redirect:/tastingnotes/list";
+    /** REDIRECT_WINE_GRAPE */
     public static final String REDIRECT_WINE_GRAPE = "redirect:/wine/grape";
+    /** REDIRECT_WINE_NEW */
     public static final String REDIRECT_WINE_NEW = "redirect:/wine/new";
+    /** REDIRECT_WISHLIST_LIST */
     public static final String REDIRECT_WISHLIST_LIST = "redirect:/wishlist/list";
+    /** REDIRECT_WELCOME */
     public static final String REDIRECT_WELCOME = "redirect:/welcome";
 
+    /** REGION_DETAILS */
     public static final String REGION_DETAILS = "region/regionDetails";
+    /** REGION_EDIT */
     public static final String REGION_EDIT = "region/regionEdit";
 
+    /** REVIEW_ADD_EDIT */
     public static final String REVIEW_ADD_EDIT = "review/reviewAddEdit";
+    /** REVIEW_LIST */
     public static final String REVIEW_LIST = "review/reviewList";
+    /** REVIEW_VIEW */
     public static final String REVIEW_VIEW = "review/reviewView";
 
+    /** SECURITY_ADMIN_CONSOLE */
     public static final String SECURITY_ADMIN_CONSOLE = "security/adminConsole";
+    /** SECURITY_BAD_TOKEN */
     public static final String SECURITY_BAD_TOKEN = "security/badToken";
+    /** SECURITY_LOGIN */
     public static final String SECURITY_LOGIN = "security/login";
+    /** SECURITY_REGISTER */
     public static final String SECURITY_REGISTER = "security/register";
+    /** SECURITY_SUCCESS */
     public static final String SECURITY_SUCCESS = "security/success";
+    /** SECURITY_USER_REGISTRATION */
     public static final String SECURITY_USER_REGISTRATION = "security/registerUser";
 
+    /** TASTED_LIST */
     public static final String TASTED_LIST = "tasted/tastedList";
 
+    /** TASTING_NOTES_ADD_EDIT */
     public static final String TASTING_NOTES_ADD_EDIT = "tastingNotes/tastingNotesAddEdit";
+    /** TASTING_NOTES_LIST */
     public static final String TASTING_NOTES_LIST = "tastingNotes/tastingNotesList";
+    /** TASTING_NOTES_VIEW */
     public static final String TASTING_NOTES_VIEW = "tastingNotes/tastingNotesView";
 
+    /** WELCOME_PAGE */
     public static final String WELCOME_PAGE = "index/welcomePage";
+    /** WINE_ADD_GRAPE */
     public static final String WINE_ADD_GRAPE = "wine/wineAddGrape";
+    /** WINE_ADD_GRAPE_BARREL */
     public static final String WINE_ADD_GRAPE_BARREL = "wine/wineAddGrapeBarrel";
+    /** WINE_DETAILS */
     public static final String WINE_DETAILS = "wine/wineDetails";
+    /** WINE_ADD_EDIT_DETAILS */
     public static final String WINE_ADD_EDIT_DETAILS = "wine/wineAddEditDetails";
+    /** WISHLIST_LIST */
     public static final String WISHLIST_LIST = "wishlist/wishlistList";
+
+    private Paths() {
+    }
 }
--- src/main/java/info/mywinecellar/nav/Session.java
+++ src/main/java/info/mywinecellar/nav/Session.java
@@ -11,13 +11,17 @@ package info.mywinecellar.nav;
 import info.mywinecellar.model.BarrelComponent;
 import info.mywinecellar.model.GrapeComponent;
 import info.mywinecellar.model.Wine;
-import org.springframework.web.bind.support.SessionStatus;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.RequestContextHolder;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+
+/**
+ * Session scope functionality
+ */
 public class Session {
 
     private Session() {
@@ -34,31 +38,59 @@ public class Session {
     private static final String GRAPE_COMPONENTS = "grapeComponents";
     private static final String BARREL_COMPONENTS = "barrelComponents";
 
+    /**
+     * Get the current country identifier
+     * @return The identifier, or <code>null</code>
+     */
     public static Long getCountryId() {
         RequestAttributes ra = RequestContextHolder.currentRequestAttributes();
         return (Long) ra.getAttribute(COUNTRY_ID, RequestAttributes.SCOPE_SESSION);
     }
 
+    /**
+     * Get the current region identifier
+     * @return The identifier, or <code>null</code>
+     */
     public static Long getRegionId() {
         RequestAttributes ra = RequestContextHolder.currentRequestAttributes();
         return (Long) ra.getAttribute(REGION_ID, RequestAttributes.SCOPE_SESSION);
     }
 
+    /**
+     * Get the current area identifier
+     * @return The identifier, or <code>null</code>
+     */
     public static Long getAreaId() {
         RequestAttributes ra = RequestContextHolder.currentRequestAttributes();
         return (Long) ra.getAttribute(AREA_ID, RequestAttributes.SCOPE_SESSION);
     }
 
+    /**
+     * Get the current producer identifier
+     * @return The identifier, or <code>null</code>
+     */
     public static Long getProducerId() {
         RequestAttributes ra = RequestContextHolder.currentRequestAttributes();
         return (Long) ra.getAttribute(PRODUCER_ID, RequestAttributes.SCOPE_SESSION);
     }
 
+    /**
+     * Get the current wine identifier
+     * @return The identifier, or <code>null</code>
+     */
     public static Long getWineId() {
         RequestAttributes ra = RequestContextHolder.currentRequestAttributes();
         return (Long) ra.getAttribute(WINE_ID, RequestAttributes.SCOPE_SESSION);
     }
 
+    /**
+     * Update the attributes
+     * @param countryId The country
+     * @param regionId The region
+     * @param areaId The area
+     * @param producerId The producer
+     * @param wineId The wine
+     */
     public static void updateSessionAttributes(Long countryId, Long regionId, Long areaId,
                                                Long producerId, Long wineId) {
         RequestAttributes ra = RequestContextHolder.currentRequestAttributes();
@@ -70,53 +102,83 @@ public class Session {
         ra.setAttribute(Session.WINE_ID, wineId, RequestAttributes.SCOPE_SESSION);
     }
 
+    /**
+     * Get the current wine
+     * @return The wine, or <code>null</code>
+     */
     public static Wine getWine() {
         RequestAttributes ra = RequestContextHolder.currentRequestAttributes();
         return (Wine) ra.getAttribute(Session.WINE, RequestAttributes.SCOPE_SESSION);
     }
 
+    /**
+     * Get the current grape component
+     * @return The grape component, or <code>null</code>
+     */
     public static GrapeComponent getGrapeComponent() {
         RequestAttributes ra = RequestContextHolder.currentRequestAttributes();
         return (GrapeComponent) ra.getAttribute(Session.GRAPE_COMPONENT, RequestAttributes.SCOPE_SESSION);
     }
 
+    /**
+     * Set the current grape component
+     * @param gc The grape component
+     */
     public static void setGrapeComponent(GrapeComponent gc) {
         RequestAttributes ra = RequestContextHolder.currentRequestAttributes();
 
         List<GrapeComponent> grapes = (List<GrapeComponent>) ra.getAttribute(Session.GRAPE_COMPONENTS,
                 RequestAttributes.SCOPE_SESSION);
 
-        if (grapes == null)
+        if (grapes == null) {
             grapes = new ArrayList<>();
+        }
 
         grapes.add(gc);
         ra.setAttribute(Session.GRAPE_COMPONENTS, grapes, RequestAttributes.SCOPE_SESSION);
 
     }
 
+    /**
+     * Get all grape components
+     * @return The grape components, or <code>null</code>
+     */
     public static List<GrapeComponent> getGrapeComponents() {
         RequestAttributes ra = RequestContextHolder.currentRequestAttributes();
         return (List<GrapeComponent>) ra.getAttribute(Session.GRAPE_COMPONENTS, RequestAttributes.SCOPE_SESSION);
     }
 
+    /**
+     * Set barrel component
+     * @param bc The barrel component
+     */
     public static void setBarrelComponent(BarrelComponent bc) {
         RequestAttributes ra = RequestContextHolder.currentRequestAttributes();
 
         List<BarrelComponent> barrels = (List<BarrelComponent>) ra.getAttribute(Session.BARREL_COMPONENTS,
                 RequestAttributes.SCOPE_SESSION);
 
-        if (barrels == null)
+        if (barrels == null) {
             barrels = new ArrayList<>();
+        }
 
         barrels.add(bc);
         ra.setAttribute(Session.BARREL_COMPONENTS, barrels, RequestAttributes.SCOPE_SESSION);
     }
 
+    /**
+     * Get all barrel components
+     * @return The barrel components, or <code>null</code>
+     */
     public static List<BarrelComponent> getBarrelComponents() {
         RequestAttributes ra = RequestContextHolder.currentRequestAttributes();
         return (List<BarrelComponent>) ra.getAttribute(Session.BARREL_COMPONENTS, RequestAttributes.SCOPE_SESSION);
     }
 
+    /**
+     * Clear
+     * @param status The session status
+     */
     public static void clear(SessionStatus status) {
         RequestAttributes ra = RequestContextHolder.currentRequestAttributes();
         ra.removeAttribute(Session.WINE, RequestAttributes.SCOPE_SESSION);
