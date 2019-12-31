53af3c111ccaf6f328cb5b6dcc1044c12fa0404f
Paul Pearson <paul@apothesource.com>
2019-12-30 21:29:20 -0500

    forgot change to sql file

--- src/main/resources/sql/01_security.sql
+++ src/main/resources/sql/01_security.sql
@@ -4,7 +4,7 @@ INSERT INTO authority (id, authority)
 VALUES (2, 'ROLE_USER');
 
 INSERT INTO users (id, enabled, username, password, first_name, last_name, email)
-VALUES (1, true, 'admin', '$2a$10$FQvwDTU8onTZ74SJy1N2zOLAeIC5em9ETr5ggcPS9/Ne/S4WXM2zG', 'Admin', 'McSecurity', 'root@mywinecellar.info');
+VALUES (1, true, 'wineadmin', '$2a$10$FQvwDTU8onTZ74SJy1N2zOLAeIC5em9ETr5ggcPS9/Ne/S4WXM2zG', 'Admin', 'McSecurity', 'root@mywinecellar.info');
 
 INSERT INTO user_authority (authority_id, user_id)
 VALUES (1, 1);
@@ -13,13 +13,13 @@ INSERT INTO user_authority (authority_id, user_id)
 VALUES (2, 1);
 
 INSERT INTO users (id, enabled, username, password, first_name, last_name, email)
-VALUES (2, true, 'user1', '$2a$10$FQvwDTU8onTZ74SJy1N2zOLAeIC5em9ETr5ggcPS9/Ne/S4WXM2zG', 'Alice', 'Wonderland', 'alice@wonderland.com');
+VALUES (2, true, 'wineuser1', '$2a$10$FQvwDTU8onTZ74SJy1N2zOLAeIC5em9ETr5ggcPS9/Ne/S4WXM2zG', 'Alice', 'Wonderland', 'alice@wonderland.com');
 
 INSERT INTO user_authority (authority_id, user_id)
 VALUES (2, 2);
 
 INSERT INTO users (id, enabled, username, password, first_name, last_name, email)
-VALUES (3, true, 'user2', '$2a$10$FQvwDTU8onTZ74SJy1N2zOLAeIC5em9ETr5ggcPS9/Ne/S4WXM2zG', 'Bob', 'Paulson', 'bob@fightclub.com');
+VALUES (3, true, 'wineuser2', '$2a$10$FQvwDTU8onTZ74SJy1N2zOLAeIC5em9ETr5ggcPS9/Ne/S4WXM2zG', 'Bob', 'Paulson', 'bob@fightclub.com');
 
 INSERT INTO user_authority (authority_id, user_id)
 VALUES (2, 3);
