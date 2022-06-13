d461525d340813ae497fa2b32f52fcf6b9f32c17
Paul Pearson <paul@apothesource.com>
2022-06-13 14:58:00 -0400

    updated spring boot to 2.7.0

--- src/main/java/info/mywinecellar/config/WebSecurityConfig.java
+++ src/main/java/info/mywinecellar/config/WebSecurityConfig.java
@@ -8,32 +8,19 @@
 
 package info.mywinecellar.config;
 
-import info.mywinecellar.service.UserService;
-
-import javax.inject.Inject;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
-    @Inject BCryptPasswordEncoder passwordEncoder;
-    @Inject UserService userService;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         // Public access to login, landing, and error pages
         http.authorizeRequests().antMatchers("/", "/login", "/errorpage").permitAll();
@@ -65,5 +52,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         // Csrf protection
         http.exceptionHandling().and().csrf().disable();
+
+        return http.build();
     }
 }
